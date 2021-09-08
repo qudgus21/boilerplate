9912721977b257a47482dbe5834420cc9b3a5c1f
qudgus21 <qudgus54321@naver.com>
2021-09-09 04:55:27 +0900

    feat: webpack 설정 추가

---
+++ client/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', 
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+
+  entry: {
+    app: './src/index.tsx',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/, 
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+  ],
+  
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+};
